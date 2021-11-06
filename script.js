36307543e4a2b372498c569cb605ec8a94ab2469
Arunkumar S <84271642+Arun140801@users.noreply.github.com>
2021-11-06 19:29:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
+// ============= variables ================== //
 
-//=====================Variables====================//
 const randomNumber = Math.floor(Math.random()*100+1)
 let chances = 5;
 let isWin = false;
 
-//=====================Variables====================//
+// ==================== variables ==================== //
 
+// =============== imports ========================= //
 
-//=======================Import=====================//
-
-const userguess = document.querySelector("#number")
+const Userguess = document.querySelector("#number")
 const resultDiv = document.querySelector(".result")
 const historyDiv = document.querySelector(".history")
 const submitButton = document.querySelector("#submit")
 
-//======================Import=====================//
 
 
+// =============== imports ========================= //
 
-//=======================main function============//
+// =================== main function ================ //
 
-const GuessingGame = () =>{
-    const guess = userguess.value;
-    
+const GuessingGame = () => {
+    const guess = Userguess.value;
     let result;
-    if(guess>100 || guess<1){
-        return resultDiv.innerHTML=<h4>$(result)</h4>
-    }else if(chances && !isWin){
-        if(randomNumber === guess){
-            result = "You Won"; 
-            isWin = true;
-        }else if(randomNumber < guess){
-            if(chances == 1){
-                result = `Game Over The number is` 
-                $(randomNumber)
-            }else{
-                result = "Your value too high";
-            isWin = false;
 
+    if (guess > 100 || guess < 1) {
+        result = "Guess a number in the range of 1 & 100"
+        resultDiv.innerHTML = `<h4>${result}</h4>`
+
+    } else if ( chances && !isWin ) {
+        if (randomNumber == guess) {
+            result = "You Won";
+            isWin = true;
+        } else if ( randomNumber < guess ) {
+            if (chances == 1) {
+                result = `Game Over, The number is ${randomNumber}`
+            } else {
+                result = "You too high! 😢";
+                isWin = false
             }
             
-        }else if(randomNumber > guess){
-            if(chances == 1){
-                result = `Game Over The number is`
-                $(randomNumber)
-            }else{
-                result = "Your value too low";
-            isWin = false;
-        }
-            
+        }else if ( randomNumber > guess ) {
+            if (chances == 1) {
+                result = `Game Over, The number is ${randomNumber}`
+            } else {
+            result = "You too low! 😢";
+            isWin = false
+            }
         }
         chances--;
-            
-        historyDiv.innerHTML += <h4> You Guessed $(guess)</h4>
-        return resultDiv.innerHTML = <h4>$(result)</h4>
-    
-    }
-//========================main function=======================//
-
-//========================reload==============================//
-    const Reload =()=>{
-        if(!chances || isWin)
-        location.reload()
+        historyDiv.innerHTML += `<h4>You Guessed ${guess}</h4>`
+        return resultDiv.innerHTML = `<h4>${result}</h4>`
     }
     
 }
-//=============================reload========================//
+
+// ================= main function =================//
+
+// ================== reload function ==============//
+
+const Reload = () => {
+    if (!chances || isWin)
+    location.reload()
+}
+
+// ================== reload function ===============//
+
+submitButton.addEventListener('click', GuessingGame)
+
